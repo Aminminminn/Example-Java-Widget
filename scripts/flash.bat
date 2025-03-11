af60fed5cce4e9946e96cb5bdf953de7f0cea33a
Amin HAJJI <101004236+Aminminminn@users.noreply.github.com>
2025-03-11 14:43:14 +0000

    Changed flash cmd to a script

---
+++ scripts/flash.bat
@@ -0,0 +1,18 @@
+@ECHO off
+
+SET CURRENT_DIRECTORY=%CD%
+ECHO "Current directory: %CURRENT_DIRECTORY%"
+
+cd ../build/application/executable/
+
+REM Need to rename the .out into .elf to call the LinkServer flash cmd
+ren application.out application.elf
+
+LinkServer flash MIMXRT1176xxxxx:MIMXRT1170-EVKB load .\application.elf
+
+IF %ERRORLEVEL% NEQ 0 (
+	ECHO "Failed to flash the board"
+	EXIT /B %ERRORLEVEL%
+)
+
+cd %CURRENT_DIRECTORY%
